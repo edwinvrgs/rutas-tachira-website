1938cbb023d88e56ca95e58c155071fc9d678e75
Edwin Vargas <onix.edwin18@gmail.com>
2016-06-16 19:53:25 -0430

    Seeders added

---
+++ database/seeds/SugerenciasTableSeeder.php
@@ -0,0 +1,16 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class SugerenciasTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //
+    }
+}
